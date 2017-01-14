02409fa52420d81393afd3118cfd97de5276b9aa
Fauziku2 <M.fauzi_87@hotmail.com>
2017-01-15 05:58:25 +0800

    basic functions working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :urls, :only => [:show, :new, :create]
+
+  # root :to => redirect('/urls/new')
+
+  root "urls#new"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
